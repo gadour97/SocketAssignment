ef304924c5f9618bd7de17d18c74e712060f17ea
gadour <31381068+gadour97@users.noreply.github.com>
2021-12-08 16:26:07 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,6 @@
+
 import java.io.*;
 import java.net.Socket;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Client {
@@ -12,21 +11,29 @@ public class Client {
 
         System.out.println("connection request");
 
-        //Create a client socket
+
         Socket socket = new Socket("127.0.0.1", port);
         System.out.println("Connection established");
 
-        // create in / out
+
         BufferedReader inClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter outClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
-        // todo: implement the scenario
 
-        //close in / out
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        String ch=scanner.nextLine() ;
+        outClient.println(ch);
+        System.out.println("Input: "+ch);
+        String ch1=inClient.readLine();
+        System.out.println("Output : "+ch1);
+
+
+
         inClient.close();
         outClient.close();
 
-        // close client socket
+
         socket.close();
     }
 }
